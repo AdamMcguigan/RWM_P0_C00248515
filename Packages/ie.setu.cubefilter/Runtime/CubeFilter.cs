2eb6ca558a5eaf49a48117e6a56bc8d2d7fef18b
Adam Mcguigan <c00248515@itcarlow.ie>
2022-09-12 17:39:43 +0100

    Added the dif filter

---
+++ Packages/ie.setu.cubefilter/Runtime/CubeFilter.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeFilter : MonoBehaviour
+{
+    public static int[] Diff(int[] xs)
+    {
+        int[] copyArray = new int[xs.Length];
+        int dif = 0;
+        int prevNum = 0;
+
+        for (int i = 0; i < xs.Length; i++)
+        {
+            int num = xs[i];
+
+            if (i == 0)
+            {
+                prevNum = 0;
+                dif = num - prevNum;
+            }
+            else
+            {
+                prevNum = xs[i - 1];
+                dif = prevNum - num;
+            }
+
+            if (dif < 0)
+            {
+                dif *= -1;
+            }
+
+            copyArray[i] = dif;
+
+        }
+        return copyArray;
+    }
+}
---
+++ Packages/ie.setu.cubefilter/Tests/CubeFilterTest.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class CubeFilterTest
+    {
+        [Test]
+        public void CubeFilterSimple()
+        {
+            int[] input = { 4, 2, 8, 3, 9, 4, 10, 5, 11, 6 };
+            int[] output = CubeFilter.Diff(input);
+            int[] expected = { 4, 2, 6, 5, 6, 5, 6, 5, 6, 5 };
+
+            CollectionAssert.AreEqual(expected, output);
+        }
+    }
+}
